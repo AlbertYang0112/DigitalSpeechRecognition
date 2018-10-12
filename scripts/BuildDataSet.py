39cc8fd58e976a7abdc0de9660b6a14c525651e2
invy <1004018919@qq.com>
2018-10-12 22:45:56 +0800

    Fix the bug in importing libs.

--- scripts/BuildDataSet.py
+++ scripts/BuildDataSet.py
@@ -1,3 +1,7 @@
+import sys
+import os
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
 from src.VoiceDataSetBuilder import VoiceDataSetBuilder
 builder = VoiceDataSetBuilder('../DataSet', rate=44100, log_file='../DataSet/DataList.txt')
 builder.build()
