6c9a0ca292df811c1be259c388836e0ca89e6f0d
invy <1004018919@qq.com>
2018-10-12 23:17:32 +0800

    Fix it again.

--- scripts/BuildDataSet.py
+++ scripts/BuildDataSet.py
@@ -1,6 +1,6 @@
 import sys
 import os
-lib_path = os.path.abspath(os.path.join('..'))
+lib_path = os.path.abspath(os.path.join(sys.path[0], '..'))
 sys.path.append(lib_path)
 from src.VoiceDataSetBuilder import VoiceDataSetBuilder
 builder = VoiceDataSetBuilder('../DataSet', rate=44100, log_file='../DataSet/DataList.txt')
