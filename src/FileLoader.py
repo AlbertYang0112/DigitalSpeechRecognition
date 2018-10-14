b2ab8b5f04c9f374f125a9e6b9ea6a0317e3d017
Albert Young <invy0112@gmail.com>
2018-10-15 01:02:08 +0800

    Revert "Add support to read a single file. Normalize function needs to be modified."
    This reverts commit 5e5c67f

--- src/FileLoader.py
+++ src/FileLoader.py
@@ -11,30 +11,16 @@ class FileLoader:
         PathToData Label
     """
 
-    def __init__(self, data_list=None):
+    def __init__(self, data_list):
         self.MUTE_LIMIT = 0.05  # Any signal below this threshold will be ignored
-        self.__dataset = None
-        self.__size = 0
-        self.set_data_list(data_list)
-        self.__data_pointer = 0
 
-    def data_set_size(self):
-        return self.__size
-
-
-    def set_data_list(self, data_list):
-        if data_list is None:
-            self.__dataset = None
-            return
         self.__dataset = []
-        self.__size = 0
         with open(data_list, 'r') as f:
             for line in f.readlines():
                 line = line.strip()
                 file_path, label = line.split(' ')
                 self.__dataset.append((file_path, label))
-                self.__size += 1
-        print("Dataset Size:", self.__size)
+        print("Dataset Size:", len(self.__dataset))
         self.__data_pointer = 0
 
     def read_next(self):
@@ -44,8 +30,6 @@ class FileLoader:
         :return: Return a tuple, consists of a 1-D numpy array containing wave data
                     and a scalar of ground truth.
         """
-        if self.__dataset is None:
-            raise IOError
         file_path = self.__dataset[self.__data_pointer][0]
         label = self.__dataset[self.__data_pointer][1]
         with wave.open(file_path, 'rb') as wavFile:
@@ -54,7 +38,7 @@ class FileLoader:
             str_data = wavFile.readframes(frame_num)
             wav_data = np.fromstring(str_data, dtype=np.int16)
             wav_min, wav_max = np.min(wav_data), np.max(wav_data)
-            wav_data = (wav_data - wav_min) * 1.0 / (1 + wav_max - wav_min)   # Normalize the wave
+            wav_data = (wav_data - wav_min) / (wav_max - wav_min)   # Normalize the wave
             wav_data = np.where(np.abs(wav_data) < self.MUTE_LIMIT,
                                 0, wav_data)
 
@@ -64,17 +48,3 @@ class FileLoader:
             self.__data_pointer += 1
 
             return wav_data, label
-
-    def read_one(self, file_name):
-        with wave.open(file_name, 'rb') as wavFile:
-            params = wavFile.getparams()
-            channels, sample_width, frame_rate, frame_num = params[:4]
-            str_data = wavFile.readframes(frame_num)
-            wav_data = np.fromstring(str_data, dtype=np.int16)
-            wav_min, wav_max = np.min(wav_data), np.max(wav_data)
-            wav_data = (wav_data - wav_min) * 1.0 / (1 + wav_max - wav_min)   # Normalize the wave
-            wav_data = np.where(np.abs(wav_data) < self.MUTE_LIMIT,
-                                0, wav_data)
-
-        return wav_data
-
