4b6810ca8c473a35dcaa4525b708d2f52c25c1f8
Albert Young <invy0112@gmail.com>
2018-10-14 22:08:58 +0800

    Add zero crossing rate, energy calculation, windowing and their unit test.

--- src/FeatureExtractors.py
+++ src/FeatureExtractors.py
@@ -1,17 +1,15 @@
 import numpy as np
-import wave
 
 
 class FeatureExtractors:
 
     @staticmethod
-    def enhance_frame(wav_data, frame_size, overlap, coefficient=0.97):
-        length = len(wav_data)
+    def enhance_frame(wav_data, frame_size, overlap, coefficient=0.97, windowing_method=None):
+        length = wav_data.shape[0]
         step = frame_size - overlap
-        frame_num = np.ceil(length / step)
+        #frame_num = np.cast(np.ceil(length / step), np.int)
+        frame_num = np.ceil(length / step).astype(np.int)
         frame_data = np.zeros((frame_size, frame_num))
-
-        window = np.hamming(frame_size)
 
         for i in range(frame_num):
             """
@@ -21,6 +19,40 @@ class FeatureExtractors:
             single_frame = wav_data[np.arange(i * step, min(i * step + frame_size, length))]
             single_frame = np.append(single_frame[0], single_frame[:-1] - coefficient * single_frame[1:])  # 预加重
             frame_data[:len(single_frame), i] = single_frame
-            frame_data[:, i] = window * frame_data[:, i]  # 加窗
+            frame_data[:, i] = FeatureExtractors.windowing(windowing_method, frame_size, frame_data[:, i])
 
         return frame_data
+
+    @staticmethod
+    def zero_crossing_rate(frames):
+        frame_num = frames.shape[1]
+        frame_size = frames.shape[0]
+        zcr = np.zeros((frame_num, 1))
+        for i in range(frame_num):
+            frame = frames[:, i]
+            temp = frame[:-1] * frame[1:]
+            temp = np.sign(temp)
+            zcr[i] = np.sum(temp < 0)
+
+        return zcr
+
+    @staticmethod
+    def energy(frames):
+        frame_num = frames.shape[1]
+        e = np.zeros((frame_num, 1))
+        for i in range(frame_num):
+            frame = frames[:, i]
+            e[i] = np.sum(frame * frame)
+
+        return e
+
+    @staticmethod
+    def windowing(method, size, frame):
+        method_dict = {
+            'Hamming': np.hamming,
+            'Rectangular': np.ones,
+        }
+        if method not in method_dict.keys():
+            return frame
+        windowing_func = method_dict[method]
+        return windowing_func(size) * frame
---
+++ test/FeatureExtractorsTests.py
@@ -0,0 +1,44 @@
+import unittest
+from src.FeatureExtractors import *
+from src.PreProcessing import *
+import matplotlib.pyplot as plt
+
+class FeatureExtractorsTests(unittest.TestCase):
+
+    def setUp(self):
+        self.WAVE_NUM = 10
+        self.WAVE_DURANCE = 1000
+        self.wave_list = np.random.randn(self.WAVE_DURANCE, self.WAVE_NUM)
+        self.base = PreProcessing(100, 10)
+        self.frames = np.array([self.base.Enframe(wave) for wave in self.wave_list])
+
+    def test_enhance_frame(self):
+        for i in range(self.WAVE_NUM):
+            self.assertTrue((
+                FeatureExtractors.enhance_frame(
+                    wav_data=self.wave_list[:, i],
+                    frame_size=100,
+                    overlap=10,
+                    windowing_method='Hamming'
+                ) ==
+                self.base.Enframe(self.wave_list[:, i])
+            ).all())
+
+    def test_zero_crossing_rate(self):
+        for i in range(self.WAVE_NUM):
+           self.assertTrue((
+               FeatureExtractors.zero_crossing_rate(
+                   frames=self.frames
+               ) ==
+               self.base.ZCR(self.frames)
+           ).any())
+
+    def test_energy(self):
+        for i in range(self.WAVE_NUM):
+            self.assertTrue((
+                FeatureExtractors.energy(self.frames) ==
+                self.base.energy(self.frames)
+            ).any())
+
+if __name__ == '__main__':
+    unittest.main()
