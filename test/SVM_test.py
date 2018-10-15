1a5b94b4e054f742d118716585e60a642cd957c2
Zirui ZHAO <864259222@qq.com>
2018-10-15 21:18:07 +0800

    Add several notes of Demo

--- test/SVM_test.py
+++ test/SVM_test.py
@@ -4,6 +4,12 @@ lib_path = os.path.abspath(os.path.join(sys.path[0], '..'))
 sys.path.append(lib_path)
 from src.SVM_Classifier import *
 
+'''
+Prerequisite: Database should be built before running demo.
+The training data cannot have multiple labels.
+Please speak loudly in a quiet place when you build the database.
+'''
+
 foldername = '../DataSet/DataList.txt' 
 
 Classifier = SVM_Classifier(foldername)
