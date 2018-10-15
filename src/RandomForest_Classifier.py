cedddd907eed708a52beb97eb7aba1c4ea136b26
Mingrui Yu <1249682625@qq.com>
2018-10-15 23:28:58 +0800

    Add files via upload

--- src/RandomForest_Classifier.py
+++ src/RandomForest_Classifier.py
@@ -1,6 +1,7 @@
-from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 import sys
 import os
+
 lib_path = os.path.abspath(os.path.join(sys.path[0], '..'))
 sys.path.append(lib_path)
 from src.PreProcessing import *
@@ -10,17 +11,19 @@ import matplotlib.pyplot as plt
 from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 
-class SVM_Classifier:
+
+class RandomForest_Classifier:
     '''
     This is the SVM_Classifier of different voice information.
     We try to utilized multiple methods to actualize the classification task
     and this is one of them.
     '''
+
     def __init__(self, DataListName):
         self.DataListName = '../DataSet/DataList.txt'
 
 
-    def read_data(self, DataListName, FeatureName,shape):
+    def read_data(self, DataListName, FeatureName, shape):
         '''
         Load original data and the feature that you choose to use is needed
         You can choose different output shape.
@@ -29,7 +32,7 @@ class SVM_Classifier:
         :shape: The output shape that you prefer
         '''
         Feature = FeatureName.capitalize()
-        Data = np.zeros((1,shape))
+        Data = np.zeros((1, shape))
         Label = []
         processer = PreProcessing(512, 128)
         wav_list, frame_list, energy_list, zcr_list, endpoint_list, Label = processer.process(DataListName)
@@ -37,14 +40,14 @@ class SVM_Classifier:
             for i in range(len(zcr_list)):
                 temp = processer.effective_feature(energy_list[i], endpoint_list[i])
                 temp = processer.reshape(temp, shape)
-                Data=np.concatenate((Data,temp),axis = 0)
+                Data = np.concatenate((Data, temp), axis=0)
             Data = Data[1:]
             return Data, Label
         elif Feature[0] == 'Z':
             for i in range(len(zcr_list)):
                 temp = processer.effective_feature(zcr_list[i], endpoint_list[i])
                 temp = processer.reshape(temp, shape)
-                Data=np.concatenate((Data,temp),axis = 0)
+                Data = np.concatenate((Data, temp), axis=0)
             Data = Data[1:]
             return Data, Label
         else:
@@ -52,16 +55,18 @@ class SVM_Classifier:
             for i in range(len(zcr_list)):
                 temp = processer.effective_feature(zcr_list[i], endpoint_list[i])
                 temp = processer.reshape(temp, shape)
-                Data=np.concatenate((Data,temp),axis = 0)
+                Data = np.concatenate((Data, temp), axis=0)
             Data = Data[1:]
             return Data, Label
-        
+
     def train(self, Data, Label):
         '''
         Train SVM model
         Feature data and labels are needed
         '''
-        clf = svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
+        shape = Data.shape[1]
+        clf = RandomForestClassifier(n_estimators=shape, max_depth=None,
+     min_samples_split=2, random_state=0)
         x_train = Data
         y_train = Label
         '''
@@ -69,16 +74,16 @@ class SVM_Classifier:
         When database is big enougth you can choose to split original database
         and set validation data.
         '''
-        #x_train, x_test, y_train, y_test = train_test_split(Data, Label, random_state=1, train_size=0.8)
+        # x_train, x_test, y_train, y_test = train_test_split(Data, Label, random_state=1, train_size=0.8)
         clf.fit(x_train, y_train)  # svm classification
         print("training result")
         print(clf.score(x_train, y_train))  # svm score
         y_hat = clf.predict(x_train)
         print("validating result")
-        #print(clf.score(x_test, y_test))
-        #y_hat = clf.predict(x_test)
+        # print(clf.score(x_test, y_test))
+        # y_hat = clf.predict(x_test)
         joblib.dump(clf, "train_model.m")
-    
+
     def apply(self, Data):
         '''
         Apply a model to predict
@@ -88,4 +93,3 @@ class SVM_Classifier:
 
     def show_accuracy(self, y_pre, y_true, Signal):
         print(Signal)
-    
