db7da6d8a5366ae460e5b4c6837330374cecb007
Mingrui Yu <1249682625@qq.com>
2018-10-15 23:30:20 +0800

    Add files via upload

---
+++ test/RandomForest_test.py
@@ -0,0 +1,21 @@
+import sys
+import os
+lib_path = os.path.abspath(os.path.join(sys.path[0], '..'))
+sys.path.append(lib_path)
+from src.RandomForest_Classifier import *
+
+'''
+Prerequisite: Database should be built before running demo.
+The training data cannot have multiple labels.
+Please speak loudly in a quiet place when you build the database.
+'''
+
+foldername = '../DataSet/DataList.txt'
+
+Classifier = RandomForest_Classifier(foldername)
+Data, Label = Classifier.read_data(foldername, 'zcr', 25)
+print(np.shape(Data))
+print(Label)
+Classifier.train(Data, Label)
+print(Classifier.apply(Data))
+print(Label)
