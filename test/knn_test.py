085b926facbbce4809e41fb1ffbd640c507399cb
1989Ryan <864259222@qq.com>
2018-10-23 20:20:07 +0800

    Add knn demo

---
+++ test/knn_test.py
@@ -0,0 +1,22 @@
+import sys
+import os
+lib_path = os.path.abspath(os.path.join(sys.path[0], '..'))
+sys.path.append(lib_path)
+from src.Classifier.KNN_Classifier import *
+
+'''
+Prerequisite: Database should be built before running demo.
+The training data cannot have multiple labels.
+Please speak loudly in a quiet place when you build the database.
+'''
+
+foldername = '../DataSet/DataList.txt' 
+
+'''
+This is the demo of decision tree classifier for comparative trial
+and the best performance is around 54%
+'''
+
+Classifier = KNN_Classifier(foldername)
+Data, Label = Classifier.read_data(foldername, 'z',20)
+Classifier.train(Data, Label)
